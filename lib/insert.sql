2e61653ed9678aae03aa09fd4b6e925fcbdeb68e
Leila Weiss <leila.weiss@powerhrg.com>
2019-04-16 09:21:28 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 27, "M", "dark brown", "Friendly", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Rowdy", 16, "M", "black", "Excited", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Tabitha", 6, "F", "dark brown", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Sargeant Brown", 19, "M", "Brown", "Intense", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Grinch", 6, "M", "Green", "Grinchy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Wendy", 13, "F", "Blue", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+(null, 18, "M", "Blue", "Aggressive", 0);
+
+
+
+
+
+
+
+
